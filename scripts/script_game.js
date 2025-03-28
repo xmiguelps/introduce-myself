b62411bc390af03b77ba75422736de7fddf2f2cb
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2025-03-28 14:39:57 -0300

    textos dos games

--- scripts/script_game.js
+++ scripts/script_game.js
@@ -22,6 +22,9 @@ function click_nav_roblox() {
     let background_atual = window.getComputedStyle(box).backgroundImage 
     if (background_atual.includes("roblox.png")) {
         add_style.href = "../styles/game_roblox.css"
+
+        box.innerHTML += "<p class='texto'>Roblox. Foi aqui que veio uma boa parte da minha infancia. Vi tantos youtubers jogando jogos incriveis e chegeu até a ver um deles dentro do mesmo jogo que eu. Joguei tantos jogos de tantos tipos que chega a ser impossivel contar no dedo, jogos esquisitos, jogos engraçados, jogos legais, jogos absurdos de bom, jogos ruins e por ai vai. E agora por ultimo mas não menos importante, foi aqui que conheci 1 pessoa que eu não podia imaginar o quão diferença esse mano podia fazer na minha vida. Esse jogo fez parte não só da minha infacia, mas da minha vida toda, e faz parte até hoje</p>"
+        
     } else if (background_atual.includes("roblox-maior.webp")) {
         back_nav(add_style, box, img, id)
     }
@@ -36,6 +39,9 @@ function click_nav_minecraft() {
     let background_atual = window.getComputedStyle(box).backgroundImage 
     if (background_atual.includes("minecraft.jpg")) {
         add_style.href = "../styles/game_minecraft.css"
+
+        box.innerHTML += "Minecraft. Um pouco menos que roblox, porem tambem teve um impacto gigante na minha vida. Antigamente, meu sonho era jogar isso em um computador, vi tantos youtubers jogando em survival, jogando minigames em servidores, e diversas series machinimas, não tem como esquecer dessas series. Minha primeira experiencia com esse jogo foi em um computador da minha prima, ali foi onde ela me deu a experiencia desse mundo incrivel, depois daquilo eu vi muito mais videos, e passei a gostar ainda mais. E hoje em dia, cá estou eu fazendo um site falando sobre ele, agora nosso minezin é o famoso jogo que a gente sente vontade de jogar por uma semana, junta os amigos e vai até todo mundo cansar"
+
     } else if (background_atual.includes("minecraft-maior.webp")) {
         back_nav(add_style, box, img, id)
     }
