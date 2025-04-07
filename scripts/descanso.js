cb39b0f1cf65b4a18000cdc3095dda60a3d5791c
Miguel Passos <182550477+xmiguelps@users.noreply.github.com>
2025-04-07 14:52:17 -0300

    audio no descanso

--- scripts/descanso.js
+++ scripts/descanso.js
@@ -1,3 +1,9 @@
-function on_music() {
-    
+function player_music() {
+    let audio = document.getElementById('caixa-de-som')
+    let img = document.getElementById('img_som')
+    if (audio.paused) {
+        audio.play()
+    } else {
+        audio.pause()
+    }
 }
